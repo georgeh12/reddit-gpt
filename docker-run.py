324e18dffd069dcad60e554a10973fce69975bb5
George Hardigg <georgeh12@gmail.com>
2024-02-26 22:07:37 -0500

    continue docker image after performing mlflow run, then adding test ml runs to the docker image

--- docker-run.py
+++ docker-run.py
@@ -1,7 +1,8 @@
 import subprocess
 
-subprocess.Popen(['mlflow', 'ui', '-h', '0.0.0.0'])
+process = subprocess.Popen(['mlflow', 'ui', '-h', '0.0.0.0'])
 
 for file in ['sentiment-analysis.ipynb', 'model-training.ipynb']:
     subprocess.run(['jupyter', 'nbconvert', '--execute', '--to', 'notebook', '--inplace', file])
 
+process.wait()
